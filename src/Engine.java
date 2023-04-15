1176052348865afdc99f93162921022b60677b0c
Superdolphin123 <130215364+Superdolphin123@users.noreply.github.com>
2023-04-15 17:32:06 +0600

    First

---
+++ src/Engine.java
@@ -0,0 +1,21 @@
+public class Engine {
+
+    Engine () {}
+
+    int[][] ground = {{0, 0, 0, 0},
+                    {0, 0, 0, 0},
+                    {0, 0, 0, 0},
+                    {0, 0, 0, 0}};
+
+    public void restart() {
+        int[][] ground = {{0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}};
+    }
+
+    public void newNum() {
+
+    }
+
+}
